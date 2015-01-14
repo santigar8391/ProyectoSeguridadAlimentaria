25680cb214480ddbafb9dc6b1cfbf5c971abb0c8
hdariosanchez <hdariosanchez@gmail.com>
2015-01-13 23:14:43 -0500

    Filtros mejorados en ubigeo de las provincias distritos....

--- app.js
+++ app.js
@@ -9,25 +9,63 @@ var express = require('express');
 var maria = require('mariasql');
 var app = express();
 
+//Router.use(app);
+/*
+* Voy a usar Jade como motor de generador de vistas... para esto
+* indico a express que mi motor de generador de vistas es JADE
+* AHORA, express siempre buscara el directorio "views" encotrar las vistas
+* y renderizarlo... ENTONCES...
+* */
+
+
+//app.use('view engine', 'jade');
+
+
+ //la siguiente linea esta por defecto para express pero lo pongo para no perderme...
+//quiere decir que le da al directorio __dirname la direccion de "views" pa que pueda encontrar y ejecutar el archivo
+//que este en el directorio...
+
+
+
+//app.use('views', __dirname + '/views');
+
 /*Linea de codigo que une el path actual desde donde sirve
 ANTES ERA... app.use("/public", express.static('/public'));
 node con express y le une "/public" */
 //app.use('/public', express.static('public'));
-app.use('/public', express.static(__dirname + '/public'));
+
+
+
+app.use('public', express.static(__dirname + '/public'));
+
+
 
 //-------------------------------------------------------------
 //Ni bien llega a lo que es la raiz que es el dominio de nuestro navegador
-//envie un response.
-app.get('/', function(req, res){
+//envie un response enviando un index.html.
 
-    res.sendfile("index.html");
-});
+//-------*****
+        app.get('/', function(req, res){
+            res.sendfile("index.html");
+        });
+//-------*****
+
+//ahora enviamos un index.jade pero con render porque es JADE
+/*app.get('/', function(req, res){
+    res.render("index,jade");
+
+});*/
 
 //Creo un servidor y lo pongo a escuchar por el puerto 7000
 app.listen(7000, function(){
     console.log("Servidor iniciado en el puerto 7000");
 });
-//-------------------------------------------
+
+
+
+
+
+//------CREANDO EL CLIENTE Y ACCEDIENDO A LA BASE DE DATOS MARIADB-------------------------------------
 var inspect = require('util').inspect;
 var Client = require('mariasql');
 
